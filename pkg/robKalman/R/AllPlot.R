c5a8c373ba73552ef0dd7ae8ab6543b639318491
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-18 15:37:24 +0000

    yet another restructuring: deleted trunc versions from wrong revision (step 1)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robkalman@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/robKalman/R/AllPlot.R
+++
@@ -1,7 +0,0 @@
-#setMethod("plot", "ParamFamily", 
-#    function(x,y=NULL,...){ 
-#        e1 <- x@distribution
-#        if(!is(e1, "UnivariateDistribution")) stop("not yet implemented")
-#
-#        plot(e1) 
-#    })
