55e00fdba16dbd1f81b912f29e9c2575fe053048
dashunka <dashunka@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-29 14:30:10 +0000

    Checking dimensions of the variables of the fct SSM is added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robkalman@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/robKalman_2012/pkg/robKalman/R/SSM-classes.R
+++ branches/robKalman_2012/pkg/robKalman/R/SSM-classes.R
@@ -2,6 +2,25 @@ SSM <- function(F, Q, Exo.state = NULL, R = NULL, distr.state = NULL,
                 Z, V, Exo.obs = NULL, T = NULL, distr.obs = NULL,
                 a0, Sigma0, Exo.ini =NULL, distr.ini = NULL,
                 p, q) {
+  # Checking dimensions of the variables of the fct
+  
+  if(! all(dim(F)==c(p,p))) print("F is of wrong dimension")
+  if(! all(dim(Z)==c(q,p))) print("Z is of wrong dimension")
+  if(! all(dim(Q)==c(p,p))) print("Q is of wrong dimension")
+  if(! all(dim(V)==c(q,q))) print("V is of wrong dimension")
+  if(! length(a0)==p) print ("a0 is of wrong dimension")
+  if(! all(dim(Sigma0)==c(p,p))) print("Sigma0 is of wrong dimension")
+  
+  if(! (length(Ex0.ini)==p|identical(Ex0.ini,NULL))) print ("Ex0.ini is of wrong dimension")
+  if(! (length(Exo.state)==p|identical(Ex0.state,NULL))) print ("Exo.state is of wrong dimension")
+  if(! (length(Exo.obs)==q|identical(Ex0.obs,NULL))) print ("Exo.obs is of wrong dimension")
+  
+  if(! (length(distr.ini)==p|identical(distr.ini,NULL))) print ("distr.ini is of wrong dimension")
+  if(! (length(distr.state)==p|identical(distr.state,NULL))) print ("distr.state is of wrong dimension")
+  if(! (length(distr.obs)==q|identical(distr.obs,NULL))) print ("distr.obs is of wrong dimension")
+  
+  #########################################################################
+  
   Exo.state.ret <- if(!is.null(Exo.state)) createExo(Exo.state) else NULL
   Exo.obs.ret <- if(!is.null(Exo.obs)) createExo(Exo.obs) else NULL
   Exo.ini.ret <- if(!is.null(Exo.ini)) createExo(Exo.ini)  else NULL
