9739fa20e0ed64ab96d95cb34028aa239d38002a
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-13 14:04:43 +0000

    Änderungen Sitzung committed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robkalman@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/robKalman_2012/pkg/robKalman/R/recFilter4.R
+++ branches/robKalman_2012/pkg/robKalman/R/recFilter4.R
@@ -6,6 +6,21 @@
 ##
 #######################################################
 
+
+     initPsRet <- function(SSM,tt, exos){
+
+        if modell has uexo uexos = matrix(...) else uexos = NULL
+        psret0 <- new("SSPredictedRet",
+                              values=matrix(0,Model@pdim,length(tt)+1),
+                              call = vector("list",length(tt)+1 ),
+                              variance = array(0,dim=c(Model@pdim,Model@pdim,length(tt)+1)),
+                              uexo = uexos,...)
+        return(psret0)
+      }
+
+## analog initPrepRet initCsRet
+
+
 recFilter <- function (Model,
                        Obs,
                        times,
@@ -36,6 +51,20 @@ recFilter <- function (Model,
      cs <- vector("list", nrSteps)
      psRet <- vector("list", nrSteps)
      csRet <- vector("list", nrSteps)
+
+     if (prep) prepret0 <- initPrepRet(SSM,tt,exox...)
+     psret0 <- initPsRet(SSM,tt,exox...)
+     csret0 <- initCsRet(SSM,tt,exox...)
+
+     for(i in nrSteps){
+           # if exists prepRet ..
+           #     prepRet0 <- new("SSPreparedRet",values=matrix)
+
+          ### aus diesem Code eine Funktion machen
+          if (prep) prepRet[[i]] <- prepret0
+          psRet[[i]] <- psret0
+          csRet[[i]] <- csret0
+     }
      for(iStep in 1:nrStep){
          psRet[[iStep]] <- initSSPredOrFiltRet(pdim = Model@pdim,
                                    qdim = Model@qdim,
@@ -103,4 +132,8 @@ recFilter <- function (Model,
             cs <- ps
          }
     }
+    zusammenhängen von list(initRet, prepRet, psRet, csRet) ## SSOutput ist jetzt
+       eine Liste mit 4 Elementen; jedes einzelne der 4 ist wieder Liste
+       und enthält zB ideal robust -> unterschied zu Output.dia
+    
  }
